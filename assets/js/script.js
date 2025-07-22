bec94a19ed36023a3b0cc19237fe4fa026f572c1
prathamHyvikk <pratham@hyvikk.com>
2025-07-22 13:57:31 +0530

    responsive navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
-const links = document.querySelectorAll('.nav-link-fancy')
 
+const hemburger_icon = document.querySelector('.hemburger_icon');
+const side_nav = document.querySelector('.side-nav');
+const menu_close = document.querySelector('.menu_close');
 
-links.map((link) => (
-    link.addEventListener()
-))
+hemburger_icon.addEventListener('click', () => {
+    side_nav.style.right = '0%'
+});
+
+menu_close.addEventListener('click', () => {
+    side_nav.style.right = '-100%'
+});
